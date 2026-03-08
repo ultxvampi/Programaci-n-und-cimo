ec19601a2eff9579d79c92ec9312a9a0fd74653f
ultxvampi <pilycerdas21@gmail.com>
2026-03-08 14:10:23 -0600

    Tarea

--- tareas/tarea001/ejercicio1.cpp
+++ tareas/tarea001/ejercicio1.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std; 
+int main(){
 /// declaro la constante de gravedad y los "valores" para poner t y y (tiempo y altura de la ventana)
-const double gravedad = 9.8; 
-double t, y;
-cout << "Ingrese el tiempo: "; ///pido el valor de t y y 
-cin >> t; 
-cout << "Ingrese la altura de la ventana: ";
-cin >> y; 
-///Mensaje de error
-if ( t <= 0){
- cout << "El tiempo no puede ser negativo " <<;
-}
-if ( y <= 0){
- cout << "La altura de la ventana no debe ser negativa " <<;
-};
-/// Calculo la velocidad (v) con ecuaciones de movimiento rectilineo uniforme acelerado ;b
-double v = (y/t) - (g*t/2);
-/// Calculo altura (h) desde ecuaciones de caída libreeeee
-double h = (v*v)/(2*g);
-cout << "La altura desde la que se lanzó fue de: " << h << "metros" <<;
+ const double gravedad = 9.8; 
+ double t, y;
+ cout << "Ingrese el tiempo:"; ///pido el valor de t y y 
+ cin >> t; 
+ cout << "Ingrese la altura de la ventana:";
+ cin >> y; 
+ ///Mensaje de error
+ if ( t <= 0){
+ cout << "El tiempo no puede ser negativo";
+ }
+ if ( y <= 0){
+ cout << "La altura de la ventana no debe ser negativa";
+ };
+ /// Calculo la velocidad (v) con ecuaciones de movimiento rectilineo uniforme acelerado ;b
+ double v = (y/t) - (g*t/2);
+ /// Calculo altura (h) desde ecuaciones de caída libreeeee
+ double h = (v*v)/(2*g);
+ cout << "La altura desde la que se lanzó fue de: " << h << "metros";
+ }
